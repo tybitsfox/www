46101836fcfac39355aba6d7ba6223085d38eb50
tybitsfox <tyyyyt@163.com>
2018-05-31 14:50:58 +0800

    2018-5-31添加博客的详细浏览页面，修改php4手册的首页显示代码

---
+++ huili/include/blog_show.php
@@ -0,0 +1,96 @@
+<?php
+if(!defined("FULL_PATH"))
+	define("FULL_PATH",substr(dirname(__FILE__),0,strlen(dirname(__FILE__))-strlen(strstr(dirname(__FILE__),"huili")))."huili".DIRECTORY_SEPARATOR);
+require_once(constant("FULL_PATH")."config/glob_new.php");	//全局常量及变量定义文件的引入
+include_once(constant("FULL_PATH")."include/head_doc.php"); //起始头文件的引入
+?>
+  <section class='promo ultrashort blog'>
+    <div class='container'>
+      <div class='promo-msg wide'>
+        <p class='sect nomargin animated'>GREENBACK BLOG</p>    
+      </div>
+    </div>
+  </section>
+  <section class='block block-blog grey'>
+    <div class='container'>
+      <div>
+        <div class='textblock widest'>
+          <div class='blogpost-article'>
+            <article class='blogpost'>
+                <header class='inner'>
+                  <p class='blog-sect'>CONNECTED ACCOUNTING</p>
+                  <h1>Save Time and Transform Your Business with Connected Accounting</h1>
+                  <p class='author'>Mar 20, 2018 </p>
+                  <div class='blog-soc'>
+                    <ul class='list-unstyled list-blogsoc'>
+                      <li>
+                        <a href='#' data-facebook='https://www.greenback.com/blog/transform-your-business-with-connected-accounting/9?utm_source=facebook&amp;utm_medium=share&amp;utm_campaign=gb-blog' class='btn'><i class='icon-wechat'></i></a>
+                      </li>
+                      <li>
+                        <a href='//twitter.com/intent/tweet?url=https%3A%2F%2Fwww.greenback.com%2Fblog%2Ftransform-your-business-with-connected-accounting%2F9%3Futm_source%3Dtwitter%26utm_medium%3Dshare%26utm_campaign%3Dgb-blog&amp;via=greenback_inc' class='btn' target='social-share'><i class='icon-tqq'></i></a>
+                      </li>
+                      <li>
+					  <?php echo"<a href='/huili/index.php?selecter=".$GLOB_DEF['PG_FUR']."' class='btn' target='social-share'><i class='icon-chevron_right'></i></a>";  ?>
+                      </li>
+                    </ul>
+                  </div>
+                </header>
+                <div class='inner wide'>
+                  <div class='blog-img'>
+                    <img src='/images/logo/transform-your-business-calculator.jpg' alt='Save Time and Transform Your Business with Connected Accounting'/>
+                  </div>
+                </div>
+              <div class='inner'>
+ <p>****   This <a href='https://startupnation.com/manage-your-business/save-time-connected-accounting/'>article</a> was originally published on <a href='https://startupnation.com/'>StartUp Nation</a> and appears here courtesy of Joe Lauer.   ****</p>
+<p><strong>Save Time and Transform Your Business with Connected Accounting</strong></p>
+<p>In order to keep up with the demands on your valuable time and ensure the future longevity of your business, it’s imperative to be automated now. The simplest way to get started is by automating your accounting tasks. You’ll save time and money by moving your accounting to the cloud, if you haven’t already.</p>
+<p>It’s easy to choose an online accounting software platform suited for your needs, and depending on your business type, some versions are even free. The most popular ones like Quickbooks Online and Xero have the most features, add-on services and third-party apps to streamline and automate your workflow. Let’s face it: when your books are in order, it directly correlates with tax-compliance and your business’ success.</p>
+<p>Cloud accounting, or connected accounting, is an intelligent choice because it is scalable, cost-effective and easy to use. It means that your business is connected to your accounting ecosystem in a closed loop. When everything is connected, bookkeeping and accounting tasks can be automated, so there is no reliance on manual data entry or problems with bottlenecks in your workflow. Structured, detailed data is captured for you from the original sources with 100 percent accuracy. There aren’t any errors caused by humans or Optical Character Recognition (OCR). Next, your flawless data is two-way synced to your accounting platform with little to no human intervention. Transactions are automatically reconciled, categorized, matched and enhanced or new ones are created, fields are populated, and original receipts, not pictures, are attached. A collaboration feature gives your accountant or employees access to your spending, and sales data on demand 24/7.</p>
+<p>According to <a href='https://www.paychex.com/articles/finance/experts-weigh-in-on-cloud-accounting'>Paychex Worx</a>, “You can improve cash flow, get better management information, and see mistakes drop while efficiency skyrockets….In short, cloud-connected accounting will transform the way your business runs.”</p>
+<p>Typically, bookkeeping is low priority for many startups, since it can be put off until the end of the month, or even the end of the year if you don’t mind penalties and fines. But in reality, it is crucial to know your financial health daily to make informed decisions quickly, and leverage your business intelligence to stay competitive.</p>
+<p>Some entrepreneurs admittedly skip bookkeeping entirely, so they don’t even know their numbers. Others use error-prone spreadsheets like Excel, which has size limits. If you want to be a pro without the work, there are new apps that can process just one or 1000s of transactions daily from high volume, multi-channel sales. There’s no need for coding, spreadsheet add-ons or employees to track it all. Receipts with an IQ includes purchases, invoices, bills, sales receipts and more showing granular data like shipping information, product images and live links. You can quickly search, export or sync transactions with line items like payments applied or multiple payments, seller fees, taxes, tips, gift or reward card payments, discounts and more.
+Your business gets smarter when you can see everything upfront and your most complicated and tedious tasks are automated. Whether you are a new startup or an established business that’s already suffered the experience and expense of hardware and software updates, IT, and security issues, there’s no need to install and run software applications on a desktop computer anymore. Your data is now bank-level protected and the internet is only a device away.</p>
+<h2 id='connected-accounting-improves-your-tax-compliance'>Connected accounting improves your tax compliance</h2>
+<p>Because you are in a closed loop accounting ecosystem, an examiner can easily observe and test internal controls if you get audited. Digital receipts are IRS approved and are actually preferred over paper since they are easy to track from the original source.</p>
+<p>An app can now capture and extract your receipt data and securely store it uncorrupted for three to seven years, depending on your subscription plan. Just be sure to consult your accountant or tax attorney for how many years you should store your data (and your insurance agent to prove expenses in case of a casualty).</p>
+<p>Remember that vendors may only store your receipts for up to two years, which isn’t long enough for the IRS. You must always be prepared for an audit even if your business survives a ransomware attack, theft, fire or natural disaster. The IRS still expects you to be completely tax-compliant. So if you want to save time, prevent lost deductions, and lower your risk for fines and penalties, make sure to have supporting documents ready. Many entrepreneurs aren’t aware that card or bank statements are not proof of payment. To claim a deduction, you must also have <a href='https://www.irs.gov/pub/irs-pdf/p583.pdf'>supporting documents</a>, “such as credit card sales slips [receipts] and invoices, to show that you also incurred the cost.” Another time-savings benefit is that an audit can be completed remotely. There’s no need to travel to an office when your accounting is in the cloud.</p>
+<h2 id='connected-accounting-protects-your-business'>Connected accounting protects your business</h2>
+<p>By aggregating your data, it is much easier to interpret your spending and sales data historically when it is all in one place. That can be difficult to do manually when it is coming from disparate sources. It allows you to notice spending patterns and see the bigger picture. There are no blind spots for an employee to hide expenses anymore. Transactions are tracked, so it is easy to locate a missing receipt. No more time lost looking for an email, or a faded piece of paper or a photo.</p>
+<h2 id='automate-your-business-now'>Automate your business now</h2>
+<p>Start benefiting from the powerful technology of connected accounting. There’s no time to waste anymore when you start automating your accounting tasks. Your business will have a competitive edge and be better prepared for the future. Look for an app in the App Store or Marketplace of your accounting software website under “Expense” or “Sales Receipt Management” that can capture original, itemized receipts from popular vendors to streamline your accounting workflow with no manual data entry, no OCR, no templates, scanning or need to snap photos.</p>
+<p><strong>About the Author</strong></p>
+<p><strong>Joe Lauer</strong> is a tech entrepreneur with more than 15 years of experience in iOS, Android, web and cloud app development, payments, social media and big data, earning several patents along the way. Currently, Lauer is Founder and CEO of <a href='https://www.greenback.com/'>Greenback</a>, a software app to automate business expenses and bookkeeping with little to no human intervention. Twitter acquired his previous company, <a href='https://www.crunchbase.com/organization/cloudhopper'>Cloudhopper</a>, in 2010 and Qpass/Amdocs acquired his <a href='https://www.crunchbase.com/organization/simplewire'>Simplewire</a> startup, one of the world’s first providers of SMS text messaging, in 2006. He holds a BSE in Computer Science from the University of Michigan College of Engineering. Lauer currently serves on the 21st Century Economy Commission for the State of Michigan.</p>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+          <section class='block block-highlight'>
+            <div class='container'>
+                <div class='inner'>
+                    <h2 class='full'>Get Started</h2>
+                    <p class='intro'></p>
+                    <div class='ctablock'>
+                        <div class='signup'>
+                            <form class='form' action='/register/signup' method='POST'>
+                                <input type='hidden' value='f807ced2-a1d9-495c-a35d-dcc95bc64240' name='authenticityToken' />
+                                <div class='form-group'>
+                                    <div class='form-prefix'>
+                                        <i class='icon-envelope picto'></i>
+                                        <input class='form-control' type='text' name='email' value='' required placeholder='Your email' />
+                                    </div>
+                                    <button type='submit' class='btn btn-primary'>Get Started</button>
+                                </div>
+                                <p>Try for free.</p>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+<?php
+//include_once(constant("FULL_PATH")."include/blog.php");
+include_once(constant("FULL_PATH")."include/foot.php");	//底部文件的引入
+?>
