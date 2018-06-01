7e74675d4f83a3890f6d700a4c71966181e0520d
tybitsfox <tyyyyt@163.com>
2018-06-01 14:27:04 +0800

    2018-6-1 备份管家数据库

---
+++ tools/hlgj_2018.sql
@@ -0,0 +1,126 @@
+-- MySQL dump 10.16  Distrib 10.1.26-MariaDB, for debian-linux-gnu (i686)
+--
+-- Host: localhost    Database: hlgj_2018
+-- ------------------------------------------------------
+-- Server version	10.1.26-MariaDB-0+deb9u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `auth`
+--
+
+DROP TABLE IF EXISTS `auth`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `auth` (
+  `uid` int(32) unsigned DEFAULT NULL COMMENT '用户ID',
+  `email` varchar(32) NOT NULL COMMENT '注册邮箱',
+  `uname` varchar(32) NOT NULL COMMENT '用户昵称',
+  `pwd` varchar(64) NOT NULL COMMENT '密码',
+  `priv` int(8) unsigned NOT NULL COMMENT '权限',
+  `lvl` int(8) unsigned NOT NULL COMMENT '等级',
+  `sex` int(8) unsigned NOT NULL COMMENT '性别',
+  `expr` varchar(64) DEFAULT NULL COMMENT '专业领域',
+  `coin` int(32) unsigned NOT NULL COMMENT '金币',
+  `treasure` int(32) unsigned NOT NULL COMMENT '财富值',
+  `lastlogin` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后登录时间',
+  `signup` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '注册时间',
+  PRIMARY KEY (`email`),
+  UNIQUE KEY `uid` (`uid`),
+  KEY `uid_name` (`uid`,`uname`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `auth`
+--
+
+LOCK TABLES `auth` WRITE;
+/*!40000 ALTER TABLE `auth` DISABLE KEYS */;
+/*!40000 ALTER TABLE `auth` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `blog`
+--
+
+DROP TABLE IF EXISTS `blog`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `blog` (
+  `tuid` int(32) unsigned NOT NULL COMMENT '帖子ID',
+  `idx` int(8) unsigned NOT NULL COMMENT '顺序号',
+  `ttile` varchar(128) NOT NULL COMMENT '帖子主题',
+  `uname` varchar(32) NOT NULL COMMENT '作者',
+  `fintime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '发帖时间',
+  `ttext` text COMMENT '内容',
+  `piclink` varchar(256) DEFAULT NULL COMMENT '图片链接',
+  `uid` int(32) unsigned NOT NULL COMMENT '作者ID',
+  PRIMARY KEY (`tuid`,`idx`),
+  KEY `name_id` (`uname`,`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `blog`
+--
+
+LOCK TABLES `blog` WRITE;
+/*!40000 ALTER TABLE `blog` DISABLE KEYS */;
+/*!40000 ALTER TABLE `blog` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `member`
+--
+
+DROP TABLE IF EXISTS `member`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `member` (
+  `uid` int(32) unsigned NOT NULL COMMENT '用户ID',
+  `uname` varchar(32) NOT NULL COMMENT '用户名',
+  `email` varchar(64) NOT NULL COMMENT '用户邮箱',
+  `pwd` varchar(256) NOT NULL COMMENT '用户密码',
+  `priv` int(8) unsigned NOT NULL COMMENT '权限',
+  `level` int(8) unsigned NOT NULL COMMENT '等级',
+  `sex` int(8) unsigned NOT NULL COMMENT '性别',
+  `expr` int(8) unsigned NOT NULL COMMENT '专业领域',
+  `coin` int(32) unsigned NOT NULL COMMENT '金币数',
+  `treasure` int(32) unsigned NOT NULL COMMENT '财富值',
+  `lastlogin` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后登录时间',
+  `signup` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '注册时间',
+  PRIMARY KEY (`email`),
+  KEY `uiduname` (`uid`,`uname`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `member`
+--
+
+LOCK TABLES `member` WRITE;
+/*!40000 ALTER TABLE `member` DISABLE KEYS */;
+/*!40000 ALTER TABLE `member` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-06-01 14:24:55
