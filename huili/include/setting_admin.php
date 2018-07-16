201da608d05098ca003b0e7da8e4337525d0f18c
tybitsfox <tyyyyt@163.com>
2018-07-16 09:22:10 +0800

    2018-7-16代码布局调整，提高功能代码块的独立性和代码的可调整性

--- huili/include/setting_admin.php
+++ huili/include/setting_admin.php
@@ -120,28 +120,7 @@ if(isset($_GET['action']) && isset($_GET['vendor']))
 ?>
 <?php
 //{{{ top fixed!
-		$st1=$_SESSION['CURR_USR'][2];
-	if(preg_match("/^[\x{4e00}-\x{9fa5}]/u",$st1))
-	{
-		$py=new pinyin();
-		$s3=$py->getpy($st1,true);
-		$st2=strtoupper(substr($s3,0,1));
-	}
-	else
-		$st2=strtoupper(substr($st1,0,1));
-		$st=sprintf($SIG_HTML['LEFT_TOP1'],$st2,$st1);
-		echo $st;
-		echo "<div class='addmerchant'><a class='btn btn-text withfronticon' href='".$SIGNED_DEF['DASHBOARD'][1][0]."'><i class='icon-arrow_left'></i>".$SIGNED_DEF['DASHBOARD'][1][1]."</a></div>";
-		echo "<ul class='list-unstyled list-nav'>\n";
-$j=count($SIGNED_DEF['PROFILE']);
-for($i=0;$i<$j;$i++)
-{
-	if(($i == 7) && ($_SESSION['CURR_USR'][0] > 100001))
-		continue;
-	$st=sprintf("<li><a href='%s'>%s<i class='%s'></i></a></li>\n",$SIGNED_DEF['PROFILE'][$i][0],$SIGNED_DEF['PROFILE'][$i][1],$SIGNED_DEF['PROFILE'][$i][2]);
-	echo $st;
-}
-		echo $SIG_HTML['LEFT_TOP3'];
+include_once('../template/def_setting.php');
 		$st2="<a href='".$SIGNED_DEF['LINK']."' >主页</a></li><li><a href='".$SIGNED_DEF['LINK']."?select=".$SIGNED_PAGE['ONE']."'>设置</a></li><li>管理员";
 		$st1=sprintf($SIG_HTML['RIGHT_TOP1'],$st2);
 		echo $st1;
--- huili/include/setting_coll.php
+++ huili/include/setting_coll.php
@@ -157,28 +157,7 @@ if(isset($_GET['action']) && ($_GET['action'] == 'true'))
 ?>
 <?php
 //{{{左边导航栏栏
-		$st1=$_SESSION['CURR_USR'][2];
-	if(preg_match("/^[\x{4e00}-\x{9fa5}]/u",$st1))
-	{
-		$py=new pinyin();
-		$s3=$py->getpy($st1,true);
-		$st2=strtoupper(substr($s3,0,1));
-	}
-	else
-		$st2=strtoupper(substr($st1,0,1));
-		$st=sprintf($SIG_HTML['LEFT_TOP1'],$st2,$st1);
-		echo $st;
-		echo "<div class='addmerchant'><a class='btn btn-text withfronticon' href='".$SIGNED_DEF['DASHBOARD'][1][0]."'><i class='icon-arrow_left'></i>".$SIGNED_DEF['DASHBOARD'][1][1]."</a></div>";
-		echo "<ul class='list-unstyled list-nav'>\n";
-$j=count($SIGNED_DEF['PROFILE']);
-for($i=0;$i<$j;$i++)
-{
-	if(($i == 5) && ($_SESSION['CURR_USR'][0] > 100001))
-		continue;
-	$st=sprintf("<li><a href='%s'>%s<i class='%s'></i></a></li>\n",$SIGNED_DEF['PROFILE'][$i][0],$SIGNED_DEF['PROFILE'][$i][1],$SIGNED_DEF['PROFILE'][$i][2]);
-	echo $st;
-}
-		echo $SIG_HTML['LEFT_TOP3'];
+include_once('../template/def_setting.php');
 //}}}
 //{{{右边显示栏抬头		div +6
 		$st2="<a href='".$SIGNED_DEF['LINK']."' >主页</a></li><li><a href='".$SIGNED_DEF['LINK']."?select=".$SIGNED_PAGE['ONE']."'>设置</a></li><li>我的合作";
