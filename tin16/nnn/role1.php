dc08f5f52767cd3e1d7d69171e536527580a26e7
tybitsfox <tyyyyt@163.com>
2018-10-27 09:58:11 +0800

    2018-10-27 update dq8

--- tin16/nnn/role1.php
+++ tin16/nnn/role1.php
@@ -35,10 +35,40 @@ echo "<tr><td>6</td><td>40</td><td>会心率上升</td><td>-</td><td>剑装备�
 echo "<tr><td>7</td><td>52</td><td>隼斩</td><td>0</td><td>以70%伤害对敌单体做出2次连续攻击,装备隼剑可4次攻击</td></tr>";
 echo "<tr><td>8</td><td>66</td><td>攻击力+25</td><td>-</td><td>剑装备时攻击力+25</td></tr>";
 echo "<tr><td>9</td><td>82</td><td>奇迹之剑</td><td>2</td><td>1</td></tr>";
-echo "<tr><td>10</td><td>100</td><td>超级斩</td><td>2</td><td>1</td></tr>";
-echo "<tr><td>组合技</td><td>-</td><td>超级重斩</td><td>2</td><td>1</td></tr>";
+echo "<tr><td>10</td><td>100</td><td>超级斩</td><td>20</td><td>对敌一组攻击的究级剑技伤害约165</td></tr>";
+echo "<tr><td>组合技</td><td>-</td><td>超级重斩</td><td>20</td><td>超级斩的强化版，伤害敌一组约300</td></tr>";
+echo "</table><br><br>";
+echo "<table width=100% >";
+echo "<tr><th colspan=5>枪技能</th></tr>";
+echo "<tr><th width=10%>等级</th><th width=10%>所需sp</th><th width=30%>技能名称</th><th width=10%>消耗MP</th><th width=40%>作用</th></tr>";
+echo "<tr><td>1</td><td>3</td><td>攻击力+5</td><td>-</td><td>枪装备时攻击力+5</td></tr>";
+echo "<tr><td>2</td><td>7</td><td>突刺</td><td>0</td><td>必定第一个攻击，伤害75%</td></tr>";
+echo "<tr><td>3</td><td>12</td><td>一闪击</td><td>3</td><td>25%会心一击、25%普通攻击，其余MISS</td></tr>";
+echo "<tr><td>4</td><td>18</td><td>攻击力+10</td><td>-</td><td>枪装备时攻击力+10</td></tr>";
+echo "<tr><td>5</td><td>25</td><td>多重击</td><td>4</td><td>以50%的伤害对敌做3～4次攻击，目标随机</td></tr>";
+echo "<tr><td>6</td><td>34</td><td>会心率上升</td><td>-</td><td>枪装备时会心一击率上升</td></tr>";
+echo "<tr><td>7</td><td>45</td><td>秋风扫</td><td>0</td><td>以90%的伤害对敌一组进行攻击伤害渐减</td></tr>";
+echo "<tr><td>8</td><td>59</td><td>雷光一闪击</td><td>3</td><td>一闪击的强化版，一旦命中必定会心一击，命中率50%</td></tr>";
+echo "<tr><td>9</td><td>77</td><td>攻击力+25</td><td>-</td><td>枪装备时攻击力+25</td></tr>";
+echo "<tr><td>10</td><td>100</td><td>雷电风暴</td><td>25</td><td>从地狱发起的对敌全体雷攻击造成伤害约200</td></tr>";
+echo "</table><br><br>";
+echo "<table width=100% >";
+echo "<tr><th colspan=5>回旋镖技能</th></tr>";
+echo "<tr><th width=10%>等级</th><th width=10%>所需sp</th><th width=30%>技能名称</th><th width=10%>消耗MP</th><th width=40%>作用</th></tr>";
+echo "<tr><td>1</td><td>6</td><td>切割投</td><td>2</td><td>敌全体攻击,，伤害85%，但最左端敌人2次伤害，第二次为第一次伤害的1/5</td></tr>";
+echo "<tr><td>2</td><td>12</td><td>攻击力+5</td><td>-</td><td>回旋镖装备时攻击力+5</td></tr>";
+echo "<tr><td>3</td><td>18</td><td>力量投</td><td>4</td><td>不会出现伤害渐减现象但伤害降低为85%</td></tr>";
+echo "<tr><td>4</td><td>25</td><td>攻击力+10</td><td>-</td><td>回旋镖装备时攻击力+10</td></tr>";
+echo "<tr><td>5</td><td>32</td><td>火焰投</td><td>6</td><td>敌全体造成炎属性伤害约35</td></tr>";
+echo "<tr><td>6</td><td>40</td><td>攻击力+15</td><td>-</td><td>回旋镖装备时攻击力+15</td></tr>";
+echo "<tr><td>7</td><td>52</td><td>强力投</td><td>4</td><td>力量投的强化版伤害提高到115%</td></tr>";
+echo "<tr><td>8</td><td>66</td><td>攻击力+20</td><td>-</td><td>回旋镖装备时攻击力+20</td></tr>";
+echo "<tr><td>9</td><td>82</td><td>星光投</td><td>8</td><td>敌全体造成光属性伤害约70</td></tr>";
+echo "<tr><td>10</td><td>100</td><td>雷光投</td><td>16</td><td>敌单体造成雷属性伤害约165</td></tr>";
+
 
 echo "<tr><td>1</td><td>4</td><td>小</td><td>2</td><td>1</td></tr>";
-
+echo "</table><br><br>";
+echo "<br><br><br><br>";
 echo "</center></div>";
 ?>
