da840bf438082ecf5693d5fc9efaf51a06195519
tybitsfox <tyyyyt@163.com>
2018-12-06 10:28:31 +0800

    2018-12-6 添加dqm的攻略

---
+++ tin16/jjj/xinde1.php
@@ -0,0 +1,38 @@
+<?php
+echo "<html><head><meta http-equiv='Content-Type' content='text/html; charset=utf-8' />";
+echo "<style type='text/css'>
+html{font-family:sans-serif;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%}
+body{ text-align:left} 
+#divcenter{margin:0 auto;width:1024px;font-family:'YaHei Consolas Hybrid'}</style>
+</head><body>";
+echo "<br><div id='divcenter'><font size=5 color=red>注：以下资料来源于百度dq吧：mys0719a 和 失误字 等几位吧友的帖子中</font></div><br>";
+echo "<div id='divcenter'><font size=4 color=blue>一、初期实用的怪物</font><br><font size=4>
+&nbsp;&nbsp;&nbsp;说一下个人的游戏心得吧，DQM1和GBC版差不多，在游戏初期可以抓<font color=green>兽系</font>和<font color=green>恶魔系</font>的怪进行配合，可以得到<font color=red>灰熊</font>，推荐<font color=red>食蚁兽</font>+<font color=red>格勒姆林</font>，<font color=red>格勒姆林</font>有荷伊米和基拉，初期比较实用的技能。<font color=red>灰熊</font>是游戏初期比较实用的怪，力量和HP成长很快，而且还有<font color=blue>疾风突</font>和<font color=blue>连续攻击</font>，对付金属史莱姆很给力，<font color=red>灰熊</font>到16级以后成长开始放慢，此时可以考虑配合掉。<font color=red>灰熊</font>也是比较好的材料怪，用<font color=green>物质系</font>和<font color=red>灰熊</font>配合可以得到<font color=red>杀人机器</font>（各属性成长都不错），<font color=red>灰熊</font>和<font color=green>史莱姆系</font>配合可以得到<font color=red>独角兽</font>（很不错的辅助治疗），需要注意的是在GBC版可以用<font color=red>杀人机器</font>和<font color=red>独角兽</font>配合得到<font color=red>狮王</font>，但是PS版不可以，可以用<font color=red>大象</font>+<font color=red>大象</font>配合得到<font color=red>狮王</font>，<font color=red>狮王</font>各属性成长非常快，而且升级速度也很快，是一周目强烈推荐的怪。<br>
+&nbsp;&nbsp;&nbsp;一周目还有一些比较不错的怪，比如用<font color=red>冰河魔神</font>和<font color=red>熔岩魔神</font>配合可以得到<font color=green>物质系</font>最强的怪<font color=red>黄金半人马</font>，成长快，升级快，抗性也比较好。还有用<font color=green>鸟系</font>和<font color=red>飞龙</font>配合可以得到<font color=red>岩石鸟</font>，同样是成长快，升级快，而且HP也比较高。再有就是<font color=red>骸骨剑士</font>和<font color=red>蛇龙战士</font>配合可以得到<font color=red>巫妖骑士</font>。<br>
+&nbsp;&nbsp;&nbsp;在初期，<font color=red>狮王</font>、<font color=red>黄金半人马</font>、<font color=red>岩石鸟</font>加上这个<font color=red>巫妖骑士</font>，都属于升级较快能力又很强的怪，是最适合通关的几个怪。还有一个<font color=green>水系</font>的怪<font color=red>大王乌贼</font>。<br>
+&nbsp;&nbsp;&nbsp;到了二周目可以配合的怪就多了，除了<font color=green>魔王系</font>的怪，各系也有非常不错的怪，比如<font color=green>史莱姆系</font>的<font color=red>黄金史莱姆</font>。<font color=red>黄金史莱姆</font>的成长短板是最大HP，成长率非常低，但是如果用最大HP999的斑点史莱姆王和最大HP999的<font color=red>黄金半人马</font>进行配合的话那么可以得到初始最大HP520+的<font color=red>黄金史莱姆</font>。二周目比较强的怪还有<font color=green>龙系</font>的<font color=red>神龙</font>，<font color=green>魔王系</font>的<font color=red>暗黑多利安</font>、<font color=red>死亡塔姆亚第三形态</font>、<font color=red>奥尔格•德米拉</font>、<font color=red>阿修罗佐玛</font>、<font color=red>佐玛</font>、<font color=red>灭绝希多</font>等等。<br>
+&nbsp;&nbsp;&nbsp;一周目的时候蛋+冬眠+牧场+身上一共可以有50只。冬眠20；身上+蛋+牧场30。</div>";
+echo "<div id='divcenter'><font size=4 color=blue>二、技能搭配</font><br><font size=4>
+&nbsp;&nbsp;&nbsp;在配技能方面，个人比较喜欢物理攻击系的<font color=blue>爆裂拳</font>，<font color=blue>连续攻击</font>四下，如果攻击力上去了，再配合倍攻的话可以达到非常不错的伤害输出，而且消耗也少，<font color=blue>爆裂拳</font>每次只消耗6点MP。其它技能是<font color=blue>查欧立克</font>，<font color=blue>贝荷玛珍</font>，<font color=blue>冻结波动</font>，这三个技能应该是必备的。然后剩下的可以选择<font color=blue>地狱雷霆</font>，<font color=blue>大爆炸</font>或者<font color=blue>玛丹代</font>，还有<font color=blue>精神统一</font>+<font color=blue>魔神斩</font>的组合。需要注意的是在GBC版中<font color=blue>爆裂拳</font>（范围爆炸）出暴击的话可以是四下全部都暴击，攻击力999时每下暴击伤害在500点左右，而PS版<font color=blue>爆裂拳</font>出暴击只有其中一下有效。<br>
+&nbsp;&nbsp;&nbsp;另外在技能继承方面，父母辈本来可以学到的技能但是由于等级没达到或者等级达到了但属性没达到要求而没能学到，此时配合掉的话他（她）的子辈还是可以学到这个技能的。举个例子，比如<font color=red>荷伊米史莱姆</font>自身可以学到<font color=blue>贝荷玛拉</font>这个技能，但是由于等级没达到20级或者达到20级了但是属性没达到要求没能学到<font color=blue>贝荷玛拉</font>技能，此时如果配合掉的话，那么配合出来的怪物只要达到20级而且属性也达到要求的话就可以学到<font color=blue>贝荷玛拉</font>技能了。但是如果配合出来的这个怪没能学到<font color=blue>贝荷玛拉</font>，再配合掉的话那么他（她）的子辈将无法学到<font color=blue>贝荷玛拉</font>技能了，所以一定要注意。<br></div>";
+echo "<div id='divcenter'><font size=4 color=blue>三、升级方面</font><br><font size=4>
+&nbsp;&nbsp;&nbsp;升级方面，DQM1一周目升级比较快的地方就是井底的那个门，抓一只<font color=red>骨囚</font>交给井底的博士就可以打开井底的那个门了。在9~11层可以遇到金属史莱姆，攻击力达到200左右可以对金属史莱姆破防，如果攻击力不够高可以用<font color=blue>魔神斩</font>或者<font color=blue>金属斩</font>。另外打倒训练师的怪经验也很多，但是训练师是随机出现的。二周目可以去全部是<font color=green>史莱姆系</font>的那个门练级，21层以上可以遇到<font color=red>失散金属</font>，攻击力450点左右可以对<font color=red>失散金属</font>破防。如果某个怪攻击力足够高的话，可以让它单刷，这样很快就可以达到等级上限了。在DQM2里，后期可以刷到一把门口全是<font color=red>黄金史莱姆</font>和金属王的钥匙，钥匙名字并不是固定的，在竞技场参加A级或S级的比赛有一定概率可以获得钥匙，A级比赛获得钥匙的概率更大一些，而且参赛费也相对便宜，得到钥匙的好坏和参赛时我方队伍中怪物的实力有关，如果竞技场前台MM推荐参加A级或S级比赛的话那么证明我方队伍中怪物的实力达到了A级或S级，此时参加这两个级别的比赛就会得到不错的钥匙。拿到钥匙后去鉴定所鉴定，如果鉴定费在1000G（最高），那么一般都可以刷出三星半以上的钥匙，在鉴定前用即时存档SAVE一下，然后鉴定，鉴定后的钥匙可以查看里面会出现哪些怪，如果不满意那么读档，通过多移动几步再和鉴定师对话或者反复进出房间来改变乱数，刷出想要的钥匙。<br>
+&nbsp;&nbsp;&nbsp;一周目到了一程度后打训练师的练级效果要比金属史莱姆高，因为训练师的怪会随着你怪物的强度而增强的，一直打训练师的话，一周目就可以打最高级的训练师了，在图书馆和徽章屋迷宫以及井底迷宫这类小的迷宫出现训练师的几率很大。二周目有失散可以打。<br>
+&nbsp;&nbsp;&nbsp;</div>";
+echo "<div id='divcenter'><font size=4 color=blue>四、怪物的配合</font><br><font size=4>
+&nbsp;&nbsp;&nbsp;两个怪物配合后新生的怪物会出现一个“+”号，“+”号后面的数字有三方面的影响，第一个是提高等级上限，比如原本这个怪的等级上限是50级，有了“+”号的话等级上限就会超过50级，每+1等级上限提高2级，等级上限最高是99级。第二个是影响成长率，“+”号后面的数字越大代表成长率越高，升级时属性提升越多。第三个是影响配合结果，比如两个史莱姆配合可以得到史莱姆王，但必须其中一个史莱姆达到+5以上才可以，否则配出来的还是史莱姆。总之“+”号后面的数字越大越好。<br>
+&nbsp;&nbsp;&nbsp;配合时父母辈怪物的等级之和会影响子辈怪物的“+”数，子辈怪物的“+”数为父母辈怪物中“+”数较高的数字加上父母辈怪物的等级之和所附加的数字，以下是归纳的数值。<br>
+父母辈怪物等级之和为20~39时+1<br>
+父母辈怪物等级之和为40~59时+2<br>
+父母辈怪物等级之和为60~79时+3<br>
+父母辈怪物等级之和为80~99时+4<br>
+父母辈怪物等级之和为100以上时+5<br>
+同系怪物之间的配合“+”数字额外+1，每次配合“+”数字最多+6。<br>
+配合时两个怪物哪个放前面做血统也是有讲究的，一般同系怪物之间的配合得到的还是放前面做血统的怪物，<font color=green>魔王系</font>的怪物和其它系的怪物配合一般得到的还是<font color=green>魔王系</font>怪物本身，除了一些特殊的配合，比如<font color=red>死亡皮萨罗</font>+<font color=red>狮王</font>=<font color=red>艾斯塔克</font>，<font color=red>艾斯塔克</font>+<font color=red>黄金史莱姆</font>=<font color=red>米尔德拉斯</font>，这些都属于特殊配合。<br></div>";
+echo "<div id='divcenter'><font size=4 color=blue>五、怪物的性格</font><br><font size=4>
+&nbsp;&nbsp;&nbsp;关于怪物的性格方面天幻攻略上没有进行详细的解释，我自己总结了一些经验，最好的怪物性格是“热血汉”，怪物的勇敢度、聪明度和温柔度这三项隐藏属性都是最高等级，怪物无论什么性格，无论什么等级都可以通过使用相应的性格书来提高这三项隐藏属性，达到“热血汉”性格。游戏中共有六种性格书，其中冒险书提高勇敢度，清醒书提高聪明度，温柔书提高温柔度，其它三种性格书都是往反方向发展的，得到后可以卖掉。使用性格书之前用即时存档SAVE一下，使用两次同样的性格书没有改变性格的话就读档换其它性格书试试，直到性格变成“热血汉”。另外战斗时通过下达不同的战术指令也会改变怪物的性格，经常使用“全力以赴”的时候怪物会增加勇敢度，性格会朝勇敢方向发展，不过也偶尔会朝愚笨方向发展。 “自由发挥”如果用的好（辅助咒文都成功的话），性格会朝聪明方向发展，如果用的不好（辅助咒文经常失败），性格就会朝狡诈方向发展了。 “保命要紧”如果用的到位（每次都回复成功或者复活同伴），就会朝温顺方向发展。如果不回复同伴或者复活同伴，就会朝胆小的方向发展，怪物的等级越高性格就越难改变。<br></div>";
+echo "<br><div id='divcenter'><font size=4>关于<font color=red>大王乌贼</font>：<font color=red>大王乌贼</font>在水之世界这个位置</font><br>
+<img src='./images/739ce9f81a4c510f0739a48e6759252dd62aa5fa.jpg' /><br>
+这是刚抓到后的能力值。回去后马上找个会回复的<font color=green>水系</font>怪转生，也是非常好用的三星半怪
+<img src='./images/52c6aad3fd1f4134d51646ea221f95cad0c85e5d.jpg' /><br></div>";
+?>
