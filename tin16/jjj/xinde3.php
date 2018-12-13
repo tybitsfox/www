549056403b3cea08adb72b0f7369f8852734e138
tybitsfox <tyyyyt@163.com>
2018-12-13 13:57:31 +0800

    2018-12-13 update dqms

--- tin16/jjj/xinde3.php
+++ tin16/jjj/xinde3.php
@@ -3,32 +3,43 @@ echo "<html><head><meta http-equiv='Content-Type' content='text/html; charset=ut
 echo "<style type='text/css'>
 html{font-family:sans-serif;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%}
 body{ text-align:left} 
-#divcenter{margin:0 auto;width:1024px;font-family:'YaHei Consolas Hybrid'}</style>
+table{ font-size:large;}
+#divcenter{margin:0 auto;width:1024px;font-family:'YaHei Consolas Hybrid'}
+.tdw{width:20%}
+.coshou{color:orange;display:inline;}
+.coniao{color:blue;display:inline;}
+.colong{color:peru;display:inline;}
+.coshi{color:green;display:inline;}
+.cozhiwu{color:brown;display:inline;}
+.coemo{color:gray;display:inline;}
+.cowuzhi{color:magenta;display:inline;}
+.cosang{color:sienna;display:inline;}
+.cowen{color:steelblue;display:inline;}
+</style>
 </head><body>";
-echo "<div id='divcenter'><font size=5>
-史莱姆+巨鸡					=					奇异蛋(阿斯特隆)<br>
-史莱姆+史莱姆				=(+5以上)			史莱姆王<br>
-斑点史莱姆+斑点史莱姆		=(+5以上)			斑点王<br><br>
-号兵象+号兵象				=					狮王<br>
-灰熊+莫扎					=					号兵象<br>
-暴牛鸟+<font color=red>恶魔系</font>				=					莫扎<br>
-<font color=blue>鸟系</font>+<font color=orange>兽系</font>					=					暴牛鸟<br><br>
-<font color=orange>兽系</font>+<font color=red>恶魔系</font>					=					灰熊<br><br>
-超级罐头+<font color=peru>龙系</font>				=					号兵象<br>
-大舌兽+格勒姆林				=					超级罐头<br>
-<font color=orange>兽系</font>+<font color=green>史莱姆系</font>				=					大舌兽<br><br>
-<font color=blue>鸟系</font>+飞龙					=					岩石鸟<br>
-<font color=peru>龙系</font>+地狱鹰					=					飞龙<br>
-莫扎+<font color=red>恶魔系</font>					=					地狱鹰<br><br>
-大舌兽+<font color=green>史莱姆系</font>				=					独角兽<br>
-灰熊+<font color=green>史莱姆系</font>				=                   独角兽<br>
-</font></div><br>";
 echo "<div id='divcenter'><table width=100% border=1 cellspacing=0 cellpadding=1>";
 echo "<tr><td width=20%><font color=blue>鸟系</font>+<font color=orange>兽系</font>=暴牛鸟<br><font color=orange>兽系</font>+<font color=red>恶魔系</font>=灰熊<br><font color=orange>兽系</font>+<font color=green>史莱姆系</font>=大舌兽<br></td>";
 echo "<td width=20%>暴牛鸟+<font color=red>恶魔系</font>=莫扎<br>大舌兽+格勒姆林=超级罐头<br>大舌兽+<font color=green>史莱姆系</font>=独角兽<br>灰熊+<font color=green>史莱姆系</font>=独角兽<br></td>";
 echo "<td width=20%>灰熊+莫扎=号兵象<br>超级罐头+<font color=peru>龙系</font>=号兵象<br>莫扎+<font color=red>恶魔系</font>=地狱鹰<br></td>";
 echo "<td width=20%>号兵象+号兵象=狮王<br><font color=peru>龙系</font>+地狱鹰=飞龙<br></td>";
 echo "<td width=20%><font color=blue>鸟系</font>+飞龙=岩石鸟<br></td>";
+echo "<tr><td class=tdw><div class=cozhiwu>植物系</div>+<div class=coemo>恶魔系</div>=邪苹果<br><div class=coemo>恶魔系</div>+<div class=colong>龙系</div>=美杜莎球<br>格勒姆林+<div class=cosang>丧尸系</div>=美杜莎球<br><div class=colong>龙系</div>+<div class=cowuzhi>物质系</div>=剑龙</td>";
+echo "<td class=tdw><div class=colong>龙系</div>+邪苹果=安德烈亚<br>美杜莎球+<div class=cosang>丧尸系</div>=独眼巨人<br><div class=cosang>丧尸系</div>+剑龙=骸骨龙</td>";
+echo "<td class=tdw><div class=cowuzhi>物质系</div>+安德烈亚=金属龙<br>独眼巨人+安德烈亚=弧光恶魔</td>";
+echo "<td class=tdw>金属龙+骸骨龙=冰河魔神<br>金属龙+弧光恶魔=熔岩魔神</td>";
+echo "<td class=tdw>冰河魔神+熔岩魔神=黄金巨像</td></tr>";
+echo "<tr><td class=tdw><div class=cosang>丧尸系</div>+<div class=colong>龙系</div>=恶灵<br>鸡冠蛇+鸡冠蛇=翼蛇<br></td>";
+echo "<td class=tdw>恶灵+<div class=colong>龙系</div>=骸骨剑士<br>翼蛇+灰熊=蛇龙战士</td>";
+echo "<td class=tdw>骸骨剑士+蛇龙战士=巫妖骑士<br>骸骨剑士+安德烈亚=苍白之主</td>";
+echo "<td class=tdw><br></td>";
+echo "<td class=tdw><br></td></tr>";
+
+echo "<tr><td class=tdw><div class=cosang>丧尸系</div>+<div class=cozhiwu>植物系</div>=木乃伊<br><div class=colong>龙系</div>+<div class=coniao>鸟系</div>=翼龙<br><div class=coshi>史莱姆系</div>+<div class=colong>龙系</div>=龙史莱姆</td>";
+echo "<td class=tdw>木乃伊+剑龙=骸骨剑士<br><div class=coniao>鸟系</div>+灰熊=暴雪鹰<br>翼龙+翼龙=魔翼龙<br><div class=coniao>鸟系</div>+龙史莱姆=吃火鸟</td>";
+echo "<td class=tdw>骸骨剑士+骸骨剑士=魔王使者<br>暴雪鹰+吃火鸟=彩虹孔雀<br>暴雪鹰+魔翼龙=彩虹孔雀</td>";
+echo "<td class=tdw>魔王使者+安德烈亚=龙王(<div class=cowen>???系</div>)<br></td>";
+echo "<td class=tdw>彩虹孔雀+<div class=cowen>???系</div>=亚兹拉尔王<br><div class=cozhiwu>植物系</div>+<div class=cowen>???系</div>=蔷薇管家</td>";
+
 echo "</tr><table></div>";
 
 echo "<div id='divcenter'><center><font size=5>系和系之间的配合表</font></center><table width=100% border=1 cellspacing=0 cellpadding=1>";
