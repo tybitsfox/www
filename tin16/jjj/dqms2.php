e240eb24179a4f50c9060370ab5176a1520fa119
tybitsfox <tyyyyt@163.com>
2019-05-19 20:57:21 +0800

    2019-5-19 update dqms12

---
+++ tin16/jjj/dqms2.php
@@ -0,0 +1,75 @@
+<?php
+echo "<html><head><meta http-equiv='Content-Type' content='text/html; charset=utf-8' />";
+echo "<style type='text/css'>
+html{font-family:sans-serif;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%}
+body{ text-align:left} 
+#divcenter{margin:0 auto;width:1024px;font-family:'YaHei Consolas Hybrid'}</style>
+</head><body>";
+echo "<br><div id='divcenter'><font size=5 color=red>M2怪物分布图</font></div><br>";
+echo "<div id='divcenter'><font size=4 color=blue>一、沙漠之世界（绿洲之钥匙）</font><br><font size=4>
+	<font color=red>大地图：</font><br>
+	斑点史莱姆，仙人球，长尾虫，鸡冠蛇，眼镜王蛇。<br>
+	<font color=red>地下水源地：</font><br>
+	大蚯蚓，鬼魂，蜡烛怪，刺球怪。</font></div>";
+echo "<div id='divcenter'><font size=4 color=blue>二、水之世界（海贼之钥匙）</font><br><font size=4>
+	<font color=red>大地图陆上：</font><br>
+	史莱姆，食蚁兽，茄子怪，多拉奇，军蚁，杀手铁锹，荷伊米史莱姆，小龙，翼龙，飞猫，跳舞萝卜，刷子小僧，独眼小丑，剧毒蜥蜴，弓箭狗，水瓢虫<br>
+	<font color=red>大地图水上：</font><br>
+	小电鳗，半人鱼，魔壶小章鱼，杀手海浪，蟾蜍食人鱼，大王乌贼。<br>
+	<font color=red>西边洞窟：</font><br>
+	大韧鱼，妖怪鱼，蟾蜍食人鱼，珍珠史莱姆<br>
+	<font color=red>东边灯塔：</font><br>
+	独眼小丑，海螺史莱姆，梅达，奇美拉<br>
+	<font color=red>东边洞穴：</font><br>
+	潜水鹰，黑暗之眼，魔杖怪，龙龟，刺球怪，八尺乌鸦，幼龙。</font></div>";
+echo "<div id='divcenter'><font size=4 color=blue>三、冰雪世界（冰之钥匙）</font><br><font size=4>
+	<font color=red>湖东之国大地图：</font><br>
+	蜥蜴人、徘徊铠甲、狐火、莫扎、角龙、四角山羊、暴牛鸟。<br>
+	<font color=red>湖东之国北山：</font><br>
+	浮游树、顽固鸟、死亡飞蛾。<br>
+	<font color=red>北之国大地图：</font><br>
+	邪苹果、翅史莱姆、野兽骑士、大木槌、耶提雪人、灰熊、魔雾、米诺恩。<br>
+	<font color=red>北之国洞穴：</font><br>
+	多洛尔、泥手、木乃伊、地狱小人。<br>
+	<font color=red>大地图水上：</font><br>
+	海马、海豚灯笼、魔鱼骑士、海狮人、海星怪<br>
+	<font color=red>西之国大地图：</font><br>
+	史莱姆骑士、魔雾、米诺恩、跳岩恶魔、奥库、狐火、<br>
+	<font color=red>南部森林：</font><br>
+	小撒旦、食人花、树桩怪、鬼影、<br>
+	<font color=red>中部魔法师塔：</font><br>
+	银恶魔、扯线木偶、石头人、金属龙、<br>
+</font></div>";
+echo "<div id='divcenter'><font size=4 color=blue>四、天空之城（天空之钥匙）：</font><br><font size=4>
+	<font color=red>中部大地图：</font><br>
+	骷髅乌鸦、邪恶邮筒、阔刀怪人、精灵鼠、杀手猩猩、<br>
+	<font color=red>东部大地图：</font><br>
+	史莱姆芳、鸸鹋怪、花魔导士、<br>
+	<font color=red>东部村庄封印塔内：</font><br>
+	铁甲龟、墨镜、食人军刀、恶魔骑士、<br>
+	<font color=red>西南部大地图：</font><br>
+	狂龙、安德烈亚、魔翼龙、<br>
+	<font color=red>西南部塔内：</font><br>
+	死亡飞盘、螺丝转鸟、铁甲蜈蚣、天空龙、地狱黄蜂、<br>
+	<font color=red>南部小洞穴：</font><br>
+	邪恶种子、大眼珠怪、巨型蜥蜴、小英雄、箱子史莱姆、<br>
+	<font color=red>中部大地图：</font><br>
+	骷髅乌鸦，邪恶邮桶，阔刀怪人，精灵鼠，螳螂战士，金属史莱姆，独眼巨人，鼯鼠，杀手猩猩，<br>
+	<font color=red>西北部大地图：</font><br>
+	杀人机器，地狱兽，金属史莱姆，翼蛇，岩石史莱姆，火龙草，安德烈亚<br>
+	<font color=red>北部大地图：</font><br>
+	岩石史莱姆，火龙草，安德烈亚，莱奥尼克<br>
+	<font color=red>东北部大地图：</font><br>
+	史莱姆芳，鸸鹋怪，花魔导士，<br>
+	<font color=red>东北部村庄塔内：</font><br>
+	铁甲龟，魔镜，食人军刀，恶魔骑士，<br>
+	<font color=red>中南部大地图：</font><br>
+	恶龙，独眼巨人，剑龙，鼯鼠，杀手鹅，吃火鸟，杀手猩猩，泡泡恶魔，<br>
+	<font color=red>西南部大地图：</font><br>
+	狂龙，安德烈亚，魔翼龙，<br>
+	<font color=red>东南部大地图：</font><br>
+	花翠鸟，暗夜飞隼，翼蛇，<br>
+	<font color=red>东南部城堡：</font><br>
+	泡沫史莱姆，死灵骑士，地狱守卫，模仿怪，<br>
+</font></div>";
+?>
