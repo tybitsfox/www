3ca168660597b2d74c2d6d6f22439309dda37eba
tybitsfox <tyyyyt@163.com>
2020-03-15 21:22:27 +0800

    2020-3-15 添加了at汇编伪指令的资料

---
+++ linux_about/asmdirective.php
@@ -0,0 +1,6 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<?php
+echo "<center><font size=5 color=#0000ff>AT&T汇编伪指令</font></center><pre>";
+include_once("./asm_directive.txt");
+echo "</pre>";
+?>
