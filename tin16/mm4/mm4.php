5e6712aa2614c1266c1262ecd4328513ad966e9f
tybitsfox <tyyyyt@163.com>
2021-11-02 11:30:42 +0800

    2021-11-2 update mm4

--- tin16/mm4/mm4.php
+++ tin16/mm4/mm4.php
@@ -4,8 +4,12 @@ echo "<html><head>
 </head><body>";
 echo "<br><center><font size=6 color=red>重装机兵4</font></center><br>";
 echo "<table align=center width=70% border=1 cellspacing=0 cellpadding=1><tr>";
-echo "<td align=center colspan=2><a href='./mm4_1.php' target=_blank><font size=4>流程篇 一</font></a></td>";
-echo "<td align=center colspan=2><a href='./mm4_2.php' target=_blank><font size=4>流程篇 二</font></a></td></tr><tr>";
+echo "<td align=center><a href='./mm4_1.php' target=_blank><font size=4>流程篇 一</font></a></td>";
+echo "<td align=center><a href='./mm4_2.php' target=_blank><font size=4>流程篇 二</font></a></td></tr><tr>";
+echo "<td align=center><a href='./mm4_3.php' target=_blank><font size=4>赏金首</font></a></td>";
+echo "<td align=center><a href='./mm4_4.php' target=_blank><font size=4>战车及装备</font></a></td></tr><tr>";
+echo "<td align=center><a href='./mm4_5.php' target=_blank><font size=4>支线任务</font></a></td>";
+echo "<td align=center><a href='./mm4_6.php' target=_blank><font size=4>人类装备</font></a></td></tr>";
 
 echo "</table><br><center><img src='./wp-content/mm4map1.jpg' border=1 style='width:90%;cursor:pointer' onclick='javascript:window.open(this.src)' /></center>";
 echo "</body>";
