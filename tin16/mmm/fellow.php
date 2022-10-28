4a0adab513b69630074d1ec3e799de6f0c001b13
tybitsfox <tyyyyt@163.com>
2022-10-28 19:22:08 +0800

    2022-10-28 update dq5攻略

--- tin16/mmm/fellow.php
+++ tin16/mmm/fellow.php
@@ -192,7 +192,84 @@ echo "<table style='width: 80%; margin-left: auto; margin-right: auto;' border='
 </table>
 <p><br /> <font size=5><span style='color: #0000cd;'>DQ5怪物同伴收服心得</span></p>
 <p><br /> &nbsp; &nbsp; &nbsp; &nbsp;所有的敌方怪物中，有一部分在战斗之后可以有一定概率让其成为伙伴加入队伍。本次DS版一共有72种怪物可以加入同伴，其中事件加入的怪物5种，随机加入的怪物67种。需要注意的一点是，当主角的级别过低时，无论如何也无法让某些怪物成为同伴，也就是说在某些场景，必须玩家的等级达到最低限度以上才可以令怪物加入。<br /> &nbsp; &nbsp; &nbsp; &nbsp;由于刷怪物全凭运气，所以很可能会有玩家连续战斗100次以上都没有收服怪物的情况发生，当然也有可能刚遇到一次就令怪物加入了同伴。总之，在自己的等级足够高的情况下，耐心的刷上1000次以上，或许才有可能让自己心仪的怪物加入队伍。<br /> &nbsp; &nbsp; &nbsp; &nbsp;在战斗中没有必要刻意去把想收服的怪物最后击破，只要战斗中遇到了该怪物，战斗之后都有一定几率令它成为伙伴。但是，如果有多个可以加入的怪物同时出现的情况下，这里面就有些小窍门了。例如战斗中史莱姆和荷米史莱姆同时出现的话，如果先打倒了史莱姆，最后打倒荷米史莱姆，战斗之后只有荷米史莱姆有可能加入。当然，史莱姆和黄鼠狼同时出现的情况下，无论先打倒史莱姆还是先打倒黄鼠狼，战斗之后只有史莱姆才有可能加入队伍，因为黄鼠狼不属于可以加入的怪物伙伴。需要注意的是，如果使用回旋镖等复数攻击武器的情况下，屏幕右边最后倒下的怪物就被默认为被最后击破的怪物。此外，即使主人公身在马车里没有加入战斗、或者主人公已经阵亡，这些情况都不会影响到怪物能否加入同伴的概率。</p></font>";
-echo "</div></body></html>";
+echo "</div><center><table style='width: 1540px; margin-left: auto; margin-right: auto;' border='1'>
+<tbody>
+<tr>
+<td><img src='./allimg/dq5/0cc18418367adab4ed3875259cd4b31c8501e4c1.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/1dfcba99a9014c08d9ca46c31d7b02087af4f438.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/1f1cb54543a98226572345419d82b9014890ebcb.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/3c24c1cec3fdfc03f7f78ddec33f8794a5c22625.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/03db938fa0ec08fa98d57dc14eee3d6d57fbdadc.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/03db938fa0ec08fa988f7dc14eee3d6d54fbda26.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/3e5867380cd79123943dfa12ba345982b0b780cb.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/03eeadc379310a5520e3b092a04543a9802610ed.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/04c738292df5e0fea8cde7674b6034a85cdf72d3.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/04c738292df5e0fea8e8e7674b6034a85fdf7238.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/4dcacafcc3cec3fd98ebc913c188d43f859427d3.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/4dcacafcc3cec3fdd9850810c188d43f86942770.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/5b89f1deb48f8c54b815ab492d292df5e2fe7fee.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/5c0bff1f4134970a7e8c7e4982cad1c8a6865d35.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/5c0bff1f4134970a7efc7e4982cad1c8a6865d25.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/5fc0952bd40735fa9d1820df89510fb30d2408c0.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/6a890db30f2442a758ec93c3c643ad4bd31302ca.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/6af7050828381f305ae6e3cfbe014c086f06f036.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/06c7bc096b63f6248b9a57659044ebf8184ca3ee.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/6dfdbe315c6034a86b6dd6e7dc13495408237625.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/8de90855b319ebc4228718ff9526cffc1f171635.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/8f519c510fb30f24c24f8265df95d143af4b03d3.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/09c74334970a304e6ad2ce9cc6c8a786c8175c38.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/25d8b8a1cd11728b18cfe244dffcc3cec1fd2cca.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/47c1e9c4b74543a915d694aa09178a82bb0114db.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/48e95e6034a85edfbb4190455e540923df5475db.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/59b6dfc451da81cb4b19edab4566d01608243135.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/65fb95dda144ad345f78e9e1c7a20cf433ad85ed.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/65fb95dda144ad345f79e9e1c7a20cf433ad85ee.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/69cefffaaf51f3de9e3cd70283eef01f3b297926.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/74d21bd8bc3eb135742fd9ddb11ea8d3ff1f44c0.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/74d21bd8bc3eb1357412d9ddb11ea8d3ff1f44d3.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/79c803e93901213faecc20ba43e736d12e2e9535.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/82d14936acaf2eddb33c278f9a1001e93b0193db.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/82d14936acaf2eddb37f278f9a1001e938019326.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/648a9a22720e0cf30e06209d1d46f21fbf09aa38.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/669dd488d43f8794cd382648c51b0ef419d53aed.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/685ad358ccbf6c81c3b9428eab3eb13532fa4026.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/734ca6c27d1ed21b62394083ba6eddc450da3f38.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/739ce9f81a4c510f3f118c7c7759252dd62aa5dc.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/788c96eef01f3a290237bba88e25bc315d607c35.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/821ea5efce1b9d16da2ff407e4deb48f8e5464c0.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/6680d309b3de9c82918797e97b81800a1bd843ca.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/54340afa513d2697922c663b42fbb2fb4216d836.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/42255066d0160924dce8cc7dc30735fae4cd34db.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/462341166d224f4af2541d751ef790529a22d1e4.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/328412385343fbf24ef9c845a77eca8067388fdc.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/a5d7dbf9d72a6059cc9f8f7c3f34349b023bba26.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/a8b836a85edf8db1fce912021e23dd54544e74ed.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/a79bf9f2b21193134eab91d672380cd793238dc1.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/ae77a60f4bfbfbed27d658196ff0f736aec31f25.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/b2fb0bf790529822a5db57a5c0ca7bcb0846d4c1.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/b5ec950a304e251fc5318a9eb086c9177d3e53ee.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/b8d06c061d950a7bb8f220a21dd162d9f0d3c9ee.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/b7718026cffc1e17104ee2575d90f603728de938.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/bb9222a4462309f74586c374650e0cf3d5cad6c0.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/be352c738bd4b31c2ce9fadd90d6277f9c2ff8dc.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/bfeeaeaf2edda3cc6d14d64616e93901203f9238.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/c1f2c45c10385343002ce9478413b07ec88088ca.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/c1f2c45c103853430030e9478413b07ecb808836.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/c20bd739b6003af3cf53b206222ac65c1238b6c0.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/c496271f95cad1c8f5769241683e6709cb3d51db.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/cdd9233fb80e7bec4c166d87382eb93899506be4.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/ce2b3887e950352a3e904b6e4443fbf2b3118b38.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/ce2b3887e950352a3eac4b6e4443fbf2b0118be4.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/daab89d4b31c87010d39dc80307f9e2f0608ff26.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/dbf6bb389b504fc2cbbdde63f2dde71192ef6dca.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/dce01d30e924b8991cde175e79061d95087bf6cb.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/e11634d3d539b600ce3261d1fe50352ac75cb735.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/f3d02a381f30e9240c69f2575b086e061f95f7e4.jpg' border='0' /></td></tr>
+<tr><td><img src='./allimg/dq5/f82c33adcbef760946e3f7f939dda3cc7ed99ec0.jpg' border='0' /></td>
+<td><img src='./allimg/dq5/f5313b01213fb80e6c040fb121d12f2ebb3894d3.jpg' border='0' /></td></tr>
+</tbody>
+</table></center>";
+echo "</body></html>";
 ?>
 <?php
 include_once("/var/www/counter.php");
