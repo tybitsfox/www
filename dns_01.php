b83bd1347cd61798a0f4ab838110fb2bab4a00f8
tybitsfox <tyyyyt@163.com>
2023-07-20 22:19:29 +0800

    20230720添加联通网盘

--- dns_01.php
+++ dns_01.php
@@ -12,6 +12,7 @@ echo "<a href='https://tybitsfox.kd.io/' target=_blank>我的koding主页</a><br
 echo "<a href='http://tybitsfox.ys168.com' target=_blank>我的永硕网盘</a><br>";
 echo "<a href='http://vdisk.weibo.com' target=_blank>我的微盘</a><br>";
 echo "<a href='https://coding.net/' target=_blank>coding</a><br>";
+echo "<a href='https://pan.wo.cn/pan/file_list/all' target=_blank>联通网盘</a><br>";
 
 //$i=read_file("./aaaa.txt");
 //echo "<br><br><br><br> test:".$i."<br>";
