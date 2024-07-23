7510e3a0ee9f53b4ae6ca14cce6860c9883c1fdb
tybitsfox <tyyyyt@163.com>
2024-07-23 15:59:52 +0800

    2024-7-23 add toys kernel memoeys map

--- kernel/kernel_mem.php
+++ kernel/kernel_mem.php
@@ -1,7 +1,45 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <?php
 echo "<center><font color=red size=4>好吧，虽然这一切都是我设计的，但我还是需要一张直观的内存分配一览表，以便更好的掌控整个内存的使用情况。</font></center><br>";
-echo "<center><font size=5 color=blue>[0M,1M)之间的内存分配</font></center><br>
+echo "<center>Updated on 2024-7-23	Version:toys</center><br>";
+echo "<center><font size=5 color=blue>[0M,1M)之间的内存分配</font></center><br><table border=0 width=90%><tr align=center>";
+echo "<td style=\"background-color:#00ff00\" width=10%>[0,0x3fff]GDT 2048*8=0x4000</td>
+<td style=\"background-color:#00ff00\" width=10%>[0x4000,0x47ff]IDT 256*8=0x800</td>
+<td style=\"background-color:#e08080\" width=10%>[0x4800,0x4fff]COIDX 256*8=0x800</td>
+<td style=\"background-color:#00ff00\" width=10%>[0x5000,0x7fff]PDT 0x1000*3=0x3000</td>
+<td style=\"background-color:yellow\" width=15%>[0x8000,0xffff]DMA 0x8000</td>
+<td style=\"background-color:gray\" width=25%>[0x10000,0x8ffff]kbuffer 512K空间</td>
+<td style=\"background-color:red\" width=10%>[0x90000,0x9efff]TSS 60*1024=0xf000</td>
+<td style=\"background-color:#0000ff\" width=10%>[0x9f000,0x9ffff]LDT 512*8=0x1000</td>
+<td style=\"background-color:#00ff00\" width=10%>[0xa0000,0xfffff]DISP&BIOS 384k</td>
+</tr></table>";
+echo "<center><font size=5 color=blue>[1M,2M)之间的内存分配</font></center><br><table border=0 width=90%><tr align=center>";
+echo "<td style=\"background-color:#00ff00\" width=20%>[0x100000,0x1007ff] INT entry functions 256*8=0x800</td>
+<td style=\"background-color:#00aaaa\" width=80%>[0x100800,0x1fffff] KERNEL code</td><tr></table><br>";
+echo "<center><font size=5 color=blue>[2M,3M)之间的内存分配</font></center><br><table border=0 width=90%><tr align=center>";
+echo "<td style=\"background-color:#00ff00\" width=10%>[0x200000,0x2000ff] struct _SYS_DATA len:0x100</td>
+<td style=\"background-color:gray\" width=10%>[0x200100,0x20ffff]  GAP	size:64k-0x100</td>
+<td style=\"background-color:#00aaaa\" width=20%>[0x200100,0x20ffff]  GAP	size:64k-0x100</td>
+
+<tr></table><br>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+echo "<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>";
+
+
+
+echo "<center><font size=5 color=blue>[0M,1M)之间的内存分配</font></center><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 <table border=0 width=90%><tr align=center>
 <td style=\"background-color:#00ff00\" width=10%>[0,0x1000)IDT 256*8=0x800</td>
 <td style=\"background-color:#00ff00\" width=10%>[0x1000,0x2000)GDT 25*8=0xC8</td>
