a989c59a59b24f288f4a72ea3b29ca8e2f80ea47
tybitsfox <tyyyyt@163.com>
2025-01-16 10:31:23 +0800

    2025-01-16 修改内存使用图

--- kernel/kernel_mem.php
+++ kernel/kernel_mem.php
@@ -56,12 +56,12 @@ echo "<td style=\"background-color:#00ff00\" width=6%>[0x200000,0x2000ff]<br> _S
 <td style=\"background-color:#00aaaa\" width=6%>[0x210000,0x210fff]<br>位图128k->4G依实际内存<br>size=0x7ff</td>
 <td style=\"background-color:orange\" width=6%>[0x211000,0x21efff]<br>内核页表2依据内存<br>size=0xdfff</td>
 <td style=\"background-color:red\" width=16%>[0x21f000,0x25Afff]<br>vm页目录表<br>依据任务数量<br>size=60*4K</td>
-<td style=\"background-color:orange\" width=25%>[0x25b000,0x2a5fff]<br>vmst结构表<br>依据任务数量<br>size=60*20*256=300K</td>
+<td style=\"background-color:orange\" width=15%>[0x25b000,0x287fff]<br>vmst结构表<br>依据任务数量<br>size=60*12*256=180K</td>
 
-<td style=\"background-color:#e5eecc\" width=35%>[0x2a6000,0x2fffff]<br>文件系统结构,total:1139k<br>size:360K</td></tr></table><br>";
+<td style=\"background-color:#e5eecc\" width=45%>[0x288000,0x2fffff]<br>文件系统结构,total:1139k<br>size:480K</td></tr></table><br>";
 echo "<center><font size=5 color=blue>[3M,4M)之间的内存分配</font></center><br><table border=0 width=90%><tr align=center>";
-echo "<td style=\"background-color:#e5eecc\" width=75%>[0x300000,0x3c2bff]<br>文件系统结构<br>size:779k</td><td style=\"background-color:gray\" width=%5>[0x3c2c00,0x3d0000)<br>GAP for mbitmap<br>size:53k</td>
-<td style=\"background-color:#e08080\" width=20%>[0x3d0000,0x3fffff]<br>待分配任务内核堆栈<br>size:192k</td></tr><tr>";
+echo "<td style=\"background-color:#e5eecc\" width=65%>[0x300000,0x3a4bff]<br>文件系统结构<br>size:659k</td><td style=\"background-color:gray\" width=%5>[0x3a4c00,0x3affff)<br>GAP for mbitmap<br>size:45k</td>
+<td style=\"background-color:#e08080\" width=30%>[0x3b0000,0x3fffff]<br>待分配任务内核堆栈<br>size:320k</td></tr><tr>";
 echo "</tr></table><br>";
 echo "<center><font size=5 color=blue>[4M,kern_mem)之间的内存分配</font></center><br><center>kern_mem的定义：内存<=16M kern_mem=4M；内存<=64M kern_mem=8M； 内存<=256M kern_mem=16M； 内存<=512M kern_mem=32M； 内存<=1024M kern_mem=64M；else kern_mem=256M<br><br></center>";
 echo "<table border=0 width=90%><tr align=center>";
