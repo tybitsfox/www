2d4ddcb044dd6933f40c8f2389c06121cbfe229f
tian <tyyyyt@163.com>
2026-07-10 19:10:06 +0800

    2026-07-10 添加virtio 相关资料

---
+++ kernel/virtio_linux.php
@@ -0,0 +1,9 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<?php
+echo "<a name=res00></a><font size=6 color=#ff0000><center>浅析virtio技术 virtqueue机制</center></font><br><br><br>";
+echo "<font size=4 color=gray><pre>
+
+
+
+";
+echo "</pre></font>";
